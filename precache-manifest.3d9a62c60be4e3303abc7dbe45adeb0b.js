4012a3119614d601e72e909cfc7b5385866bd3bf
artem-meleshko <timonlondon@hotmail.com>
2019-06-29 13:20:14 +0300

    Updates

---
+++ precache-manifest.3d9a62c60be4e3303abc7dbe45adeb0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd52e503b89072efc255cfe0a356f5b5",
+    "url": "/cra-mate1/index.html"
+  },
+  {
+    "revision": "b57d08fecfaeae1532da",
+    "url": "/cra-mate1/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/cra-mate1/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b57d08fecfaeae1532da",
+    "url": "/cra-mate1/static/js/main.ebb56893.chunk.js"
+  },
+  {
+    "revision": "a9d11e884e1b63f63442",
+    "url": "/cra-mate1/static/js/runtime~main.0cd30b19.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cra-mate1/static/media/logo.5d5d9eef.svg"
+  }
+]);
